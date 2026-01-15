bbd457f9e2d4654a71181e200c15de7a255d4ab2
sinap55 <parvanehs@limestone.on.ca>
2026-01-15 12:13:10 -0500

    Created password checker (terminal based)

--- FinalProject.ino
+++ FinalProject.ino
@@ -11,9 +11,14 @@ int CLK = 13;
 LedControl lc = LedControl(DIN, CLK, CS, 2);
 
 int btnPins[] = { 5, 4, 3, 2 };
-int guesses[4] = {0,0,0,0};
+int guesses[4] = { 0, 0, 0, 0 };
+
+int password[4];
+
+int passBtnPin = 8;
 
 int buttonState;
+int passBtnState;
 
 // grid to create number designs universally
 int grid[8];
@@ -26,24 +31,37 @@ int* right;
 
 
 void setup() {
+  
+  Serial.begin(9600);
+  randomSeed(analogRead(A0));
+
+
+
+  Serial.println("Password:");
+  for (int i = 0; i < 4; i++) {
+    password[i] = random(10);
+    Serial.print(password[i]);
+  }
+  
   //LC SET UP
   lc.shutdown(0, false);  // When the device comes out of shutdown mode the same Leds will be lit as before it went to sleep
   lc.shutdown(1, false);
-  lc.setIntensity(0, 1);  // set brightness
-  lc.setIntensity(1, 1);
+  lc.setIntensity(0, 15);  // set brightness
+  lc.setIntensity(1, 15);
 
   pinMode(btnPins[0], INPUT);
   pinMode(btnPins[1], INPUT);
   pinMode(btnPins[2], INPUT);
   pinMode(btnPins[3], INPUT);
+
+  pinMode(passBtnPin, INPUT);
 }
 
 void loop() {
   printGrid(0, guesses[0], guesses[1]);
   printGrid(1, guesses[2], guesses[3]);
-  
+  passwordChecker();
   btnRead();
-
 }
 
 void printGrid(int gridselectnum, int leftnum, int rightnum) {
@@ -68,7 +86,7 @@ void btnRead() {
 
   for (int i = 0; i < 4; i++) {
     buttonState = digitalRead(btnPins[i]);
-    delay(50);
+    delay(25);
     if (buttonState == HIGH) {
       guesses[i] = guesses[i] + 1;
       if (guesses[i] > 9) {
@@ -77,4 +95,27 @@ void btnRead() {
     }
   }
 }
-  
+
+
+void passwordChecker() {
+  passBtnState = digitalRead(passBtnPin);
+  delay(20);
+  //Serial.println(digitalRead(passBtnPin));
+  if (passBtnState == HIGH) {
+    bool correct = true;
+
+    for (int i = 0; i < 4; i++) {
+      if (guesses[i] != password[i]) {
+        correct = false;
+      }
+    }
+
+    if (correct) {
+      Serial.println("Correct password!");
+      // motor stuff here
+    } else {
+      Serial.println("Incorrect password!");
+    }
+  }
+}
+
